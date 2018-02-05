552c52e464bca791b617538075d5751b00e86013
Marian Meres <marian@meres.sk>
2018-02-05 20:27:01 +0100

    package.json

---
+++ dist/mm-date.js
@@ -0,0 +1,61 @@
+import { isDate, isNaN } from 'lodash';
+/**
+ * @param date
+ * @returns {boolean}
+ */
+export function mmIsValidDate(date) {
+    return (isDate(date) && !isNaN(date.valueOf()));
+}
+/**
+ * @param year
+ * @param month
+ * @returns {number}
+ */
+export function mmGetDaysInMonth(year, month) {
+    return (32 - new Date(year, month, 32).getDate());
+}
+/**
+ * @param year
+ * @param month
+ * @returns {number}
+ */
+export function mmGetStartWeekday(year, month) {
+    return new Date(year, month, 1).getDay();
+}
+/**
+ * @param year
+ * @returns {boolean}
+ */
+export function mmIsLeapYear(year) {
+    return new Date(year, 1, 29).getMonth() === 1;
+}
+/**
+ * @param d1
+ * @param d2
+ * @returns {Object|boolean}
+ */
+export function mmIsSameDay(d1, d2) {
+    return (mmIsValidDate(d1)
+        && mmIsValidDate(d2)
+        && d1.getFullYear() === d2.getFullYear()
+        && d1.getMonth() === d2.getMonth()
+        && d1.getDate() === d2.getDate());
+}
+/**
+ * @param d1
+ * @param d2
+ * @returns {Object|boolean}
+ */
+export function mmIsSameMonth(d1, d2) {
+    return (mmIsValidDate(d1)
+        && mmIsValidDate(d2)
+        && d1.getFullYear() === d2.getFullYear()
+        && d1.getMonth() === d2.getMonth());
+}
+/**
+ * @param d
+ * @returns {boolean}
+ */
+export function mmIsToday(d) {
+    return mmIsSameDay(d, new Date());
+}
