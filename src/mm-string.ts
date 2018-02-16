595e7740d772431922a8aa41bf8e046690066a74
Marian Meres <marian@meres.sk>
2018-02-16 09:25:06 +0100

    string mmUnaccent

--- src/mm-string.ts
+++ src/mm-string.ts
@@ -163,6 +163,7 @@ export function mmReplaceMap(str, map, ignoreCase = false) {
 }
 
 /**
+ * https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
  * I guess more robust alternative: https://github.com/andrewrk/node-diacritics.git
  * @param str
  */
